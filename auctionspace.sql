3211d1f5492731715e62b39ca23c44e715c4e52c
Shipra Shinde <shipra.ss17@gmail.com>
2017-03-31 14:56:50 -0400

    Adding sql file

---
+++ auctionspace.sql
@@ -0,0 +1,13 @@
+CREATE TABLE items (item_display_name VARCHAR(500), price FLOAT, quantity INT, start_time DATE, end_time DATE, location VARCHAR(100), description VARCHAR(1000));
+
+CREATE TABLE User (
+fname varchar(25),
+mname varchar(25),
+lname varchar(25),
+emailId varchar(30),
+username varchar(40),
+password varchar(40),
+phone int,
+address varchar(50),
+userType varchar(10)
+);
